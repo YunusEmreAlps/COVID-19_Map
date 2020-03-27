8032b31932aa65be31d038d849a6d5228608e9eb
Yunus Emre Alpu <yunus192alpu@gmail.com>
2020-03-27 23:41:58 +0300

    add a chart

--- script.js
+++ script.js
@@ -1,57 +1,50 @@
-// Chart Component
-Vue.component('bar-chart', {
-    extends: VueChartJs.Bar,
-    // data 
-    data: function() {
-        return {
-            datacollection: {
-                labels: ['Day'],
-                datasets: [
-                    {
-                        label: 'COVID-19 Data',
-                        backgroundColor: '#f87979',
-                        pointBackgroundColor: 'white',
-                        borderWidth: 1,
-                        pointBorderColor: '#249EBF',
-                        data: []
-                    },
-                ]
-            },
-        }
+Vue.component("line-chart", {
+    extends: VueChartJs.Radar,
+    props: ["data", "label", "options"], // connection !important
+    mounted() { 
+      this.renderLineChart();
     },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                },
-                gridLines: {
-                    display: true
-                }
-            }],
-            xAxes: [{
-                ticks: {
-                    beginAtZero: true
-                },
-                gridLines: {
-                    display: true
-                }
-            }],
+    // update data 
+    computed: {
+      chartData: function() {
+        return this.data;
+      },
+      chartLabel: function() {
+        return this.label;
+      }
+    },
+    methods: {
+      renderLineChart: function() {
+      this.renderChart(
+        {
+          labels:this.chartLabel,
+          datasets: [
+            {
+              label: "COVID-19 Data",
+              backgroundColor: "#f87979",
+              data: this.chartData
+            }
+          ]
         },
-        legend: {
-            display: false
-        },
+        { responsive: true, maintainAspectRatio: false }
+      );      
+      }
     },
-    mounted () {
-        // this.chartData is created in the mixin
-        this.renderChart(this.datacollection, this.options)
-    },
-});
+   /* watch: {
+      data: function() {
+        this._chart.destroy();
+        //this.renderChart(this.data, this.options);
+        this.renderLineChart();
+      }
+    }*/
+  });
+
 
 // Vue.js
 var app = new Vue({
     el:"#app", // element
     data: {
+        cont: false,
         totalc: 0, // total cases
         totalr: 0, // total recovered
         totalac: 0, // total active cases
@@ -62,17 +55,21 @@ var app = new Vue({
         countries: [],
         styleObject: {
             fontSize: '16px'
-        }
+        }, 
+        hdata: [],
+        dataChart: [],
+        labelChart: ['Cases','Recovered','Active','Deaths','Critical','Today Cases']
     },
     created () {
         this.gdata();
+        this.hisdata();
     },
     computed: {
         filteredcntr: function(){
             return this.countries.filter((cntry) =>{
                return cntry.country.match(this.srch);
             });
-        }
+          }
     },
     methods: {
         gdata(){
@@ -87,8 +84,32 @@ var app = new Vue({
                     this.totald+=this.countries[i].deaths;
                     this.tcritic+=this.countries[i].critical;
                     this.todcas+=this.countries[i].todayCases;
-                } 
+                }
+                this.dataChart.push(this.totalc);
+                this.dataChart.push(this.totalr);
+                this.dataChart.push(this.totalac);
+                this.dataChart.push(this.totald);
+                this.dataChart.push(this.tcritic);
+                this.dataChart.push(this.todcas);
+                this.cont=true;
             });
+
         },
+        hisdata() {
+          fetch('https://corona.lmao.ninja/v2/historical/')
+          .then(Response => Response.json())
+          .then(Response => {
+              for(var i=0;i<Response.length; i++){ 
+                this.hdata.push(Response[i].timeline.deaths);
+              }                  
+          });
+        },
+        changeData: function(e) {
+          // e = e.charAt(0).toUpperCase()+e.slice(1);
+          //for(x in this.hdata){ 
+            //this.labelChart.push(x);
+            // this.dataChart.push(Response.timeline.deaths[x]);
+          //}
+        }
     },
 });
